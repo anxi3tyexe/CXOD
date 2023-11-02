12ac258081a9beadda0d97e869da67c1e78dc1ca
ISIXONEISIXONE <i027356@inbox.ru>
2023-11-02 20:20:08 +0700

    trpc added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,123 +28,123 @@ export default function Home() {
         >
           Начнем <ArrowRight className='ml-2 h-5 w-5' />
         </Link>
-      </MaxWidthWrapper>
 
-      {/* Value Proposition Section */}
-      <div>
-        <div className='relative isolate'>
-          <div
-            aria-hidden='true'
-            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-          >
+        {/* Value Proposition Section */}
+        <div>
+          <div className='relative isolate'>
             <div
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-              className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1.5 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#cd4e4e] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
-            />
-          </div>
+              aria-hidden='true'
+              className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1.5 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#cd4e4e] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]'
+              />
+            </div>
 
-          <div>
-            <div className='mx-auto max-w-6xl px-6 lg:px-8'>
-              <div className='mt-16 flow-root sm:mt-24'>
-                <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
-                  <Image
-                    alt='some img'
-                    src='/dashboard-preview.jpg'
-                    width={1364}
-                    height={866}
-                    quality={100}
-                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
-                  />
+            <div>
+              <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+                <div className='mt-16 flow-root sm:mt-24'>
+                  <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                    <Image
+                      alt='some img'
+                      src='/dashboard-preview.jpg'
+                      width={1364}
+                      height={866}
+                      quality={100}
+                      className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          <div
-            aria-hidden='true'
-            className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
-          >
             <div
-              style={{
-                clipPath:
-                  "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-              }}
-              className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1.5 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#cd4e4e] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
-            />
-          </div>
-        </div>
-      </div>
-
-      {/*Feature section*/}
-      <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
-        <div className='mb-12 px-6 lg:px-8'>
-          <div className='mx-auto max-w-2xl sm:text-center'>
-            <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
-              Начните в считанные секунды
-            </h2>
-            <p className='mt-4 text-lg text-gray-600'>
-              Juliett - все что нужно для беседы
-            </p>
-          </div>
-        </div>
-
-        {/* steps */}
-        <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
-          <li className='md:flex-1'>
-            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
-              <span className='text-sm font-medium text-rose-600'>Шаг 1</span>
-              <span className='text-xl font-semibold'>Зарегистрируйтесь</span>
-              <span className='mt-2 text-zinc-700'>
-                Выберите тариф - бесплатный или{" "}
-                <Link
-                  href='/pricing'
-                  className='text-rose-700 underline underline-offset-2'
-                >
-                  PRO
-                </Link>
-              </span>
-            </div>
-          </li>
-          <li className='md:flex-1'>
-            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
-              <span className='text-sm font-medium text-rose-600'>Шаг 2</span>
-              <span className='text-xl font-semibold'>
-                Загрузите свой PDF документ
-              </span>
-              <span className='mt-2 text-zinc-700'>
-                А Juliett позаботится об остальном
-              </span>
-            </div>
-          </li>
-          <li className='md:flex-1'>
-            <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
-              <span className='text-sm font-medium text-rose-600'>Шаг 3</span>
-              <span className='text-xl font-semibold'>Задавайте вопросы</span>
-              <span className='mt-2 text-zinc-700'>
-                Вы будете удивлены: это так просто!
-              </span>
-            </div>
-          </li>
-        </ol>
-
-        <div className='mx-auto max-w-6xl px-6 lg:px-8'>
-          <div className='mt-16 flow-root sm:mt-24'>
-            <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
-              <Image
-                alt='Uploading Preview'
-                src='/file-upload-preview.jpg'
-                width={1419}
-                height={732}
-                quality={100}
-                className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+              aria-hidden='true'
+              className='pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
+            >
+              <div
+                style={{
+                  clipPath:
+                    "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+                }}
+                className='relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1.5 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#cd4e4e] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]'
               />
             </div>
           </div>
         </div>
-      </div>
+
+        {/*Feature section*/}
+        <div className='mx-auto mb-32 mt-32 max-w-5xl sm:mt-56'>
+          <div className='mb-12 px-6 lg:px-8'>
+            <div className='mx-auto max-w-2xl sm:text-center'>
+              <h2 className='mt-2 font-bold text-4xl text-gray-900 sm:text-5xl'>
+                Начните в считанные секунды
+              </h2>
+              <p className='mt-4 text-lg text-gray-600'>
+                Juliett - все что нужно для беседы
+              </p>
+            </div>
+          </div>
+
+          {/* steps */}
+          <ol className='my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0'>
+            <li className='md:flex-1'>
+              <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+                <span className='text-sm font-medium text-rose-600'>Шаг 1</span>
+                <span className='text-xl font-semibold'>Зарегистрируйтесь</span>
+                <span className='mt-2 text-zinc-700'>
+                  Выберите тариф - бесплатный или{" "}
+                  <Link
+                    href='/pricing'
+                    className='text-rose-700 underline underline-offset-2'
+                  >
+                    PRO
+                  </Link>
+                </span>
+              </div>
+            </li>
+            <li className='md:flex-1'>
+              <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+                <span className='text-sm font-medium text-rose-600'>Шаг 2</span>
+                <span className='text-xl font-semibold'>
+                  Загрузите свой PDF документ
+                </span>
+                <span className='mt-2 text-zinc-700'>
+                  А Juliett позаботится об остальном
+                </span>
+              </div>
+            </li>
+            <li className='md:flex-1'>
+              <div className='flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:border-t-2 md:pb-0 md:pl-0 md:pt-4'>
+                <span className='text-sm font-medium text-rose-600'>Шаг 3</span>
+                <span className='text-xl font-semibold'>Задавайте вопросы</span>
+                <span className='mt-2 text-zinc-700'>
+                  Вы будете удивлены: это так просто!
+                </span>
+              </div>
+            </li>
+          </ol>
+
+          <div className='mx-auto max-w-6xl px-6 lg:px-8'>
+            <div className='mt-16 flow-root sm:mt-24'>
+              <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <Image
+                  alt='Uploading Preview'
+                  src='/file-upload-preview.jpg'
+                  width={1419}
+                  height={732}
+                  quality={100}
+                  className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </MaxWidthWrapper>
     </>
   );
 }
